5ca4699ac452e433b55fc603a6ea5a032d2a3744
AnneMaier <hhch98@gmail.com>
2025-08-18 13:32:01 +0900

    KT1-149: fix(garden): 개인화 앨범에서 name 컬럼 표시 제거

--- garden-of-memories/src/App.jsx
+++ garden-of-memories/src/App.jsx
@@ -16,7 +16,7 @@ import HomePage from './pages/HomePage/HomePage';
 import NotificationPage from './pages/NotificationPage/NotificationPage';
 import WeeklyReportPage from './pages/WeeklyReportPage/WeeklyReportPage';
 import ReportDetailPage from './pages/ReportDetailPage/ReportDetailPage';
-import EnterPage from './pages/EnterPage/EnterPage';
+
 
 function App() {
   return (
@@ -39,7 +39,7 @@ function App() {
         <Route path="/story-game-dashboard" element={<StoryGameDashboard />} />
         <Route path="/garden" element={<GardenPage />} />
         <Route path="/story-sequence" element={<StorySequence />} />
-        <Route path="/enter" element={<EnterPage />} />
+        
         <Route path="/notifications" element={<NotificationPage />} />
         <Route path="/reports" element={<WeeklyReportPage />} />
         <Route path="/reports/:reportId" element={<ReportDetailPage />} />
